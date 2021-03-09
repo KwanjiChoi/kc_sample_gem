b7b48ec7875d754c172a1536499399a88c96bbad
KwanjiChoi <kwanji0921@gmail.com>
2021-03-09 17:44:49 +0900

    add get cities method

--- lib/kc_sample_gem.rb
+++ lib/kc_sample_gem.rb
@@ -1,6 +1,30 @@
 require "kc_sample_gem/version"
+require 'net/http'
+require 'json'
 
 module KcSampleGem
   class Error < StandardError; end
   # Your code goes here...
+
+
+  BASE_URL = 'http://geoapi.heartrails.com/api/json'.freeze
+
+  def self.greeting
+    puts 'Hello Kwanji!'
+  end
+
+  def self.get_cities(prefecture)
+
+    uri = URI.parse(BASE_URL)
+    params = {
+      method: 'getCities',
+      prefecture: prefecture,
+    }
+    uri.query = URI.encode_www_form(params)
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = uri.scheme === "https"
+    response = http.get(uri.request_uri)
+    JSON.parse(response.body)['response']['location']
+  end
+
 end
